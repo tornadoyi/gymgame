a3b7b1d5e221127ae9940271eaad37c4380477eb
Jason <jason@Jasons-MacBook-Pro.local>
2017-07-04 09:58:13 +0800

    add config demo for men test

---
+++ test/man/config.py
@@ -0,0 +1,29 @@
+
+
+from gymgame.engine import Vector2, extension
+from gymgame.tinyrpg import man
+
+config = man.config
+
+Attr = config.Attr
+
+GAME_NAME = config.GAME_NAME
+
+
+# env constant
+config.MAP_SIZE = Vector2(10, 10)
+config.GRID_SIZE = config.MAP_SIZE
+
+
+@extension(man.Serializer)
+class SerializerExtension():
+
+    DIRECTS = [Vector2.up, Vector2.right, Vector2.down, Vector2.left]
+
+    def _deserialize_action(self, data):
+        direct = SerializerExtension.DIRECTS[data]
+        actions = [('player-0', config.Action.move_toward, direct, None)]
+        return actions
+
+
+
